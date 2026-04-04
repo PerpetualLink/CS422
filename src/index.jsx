af9cc6b7d9e7fc5a2241aa9909cfca6d76701b26
PerpetualLink <Samack1011@gmail.com>
2026-04-04 01:44:15 -0500

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>Hello world 🌍</h1>
+    <h2>Welcome to React 🎉</h2>
+  </div>
+); 
