469a8d11c1c7ea8999ec6cca592103eb08031e71
PerpetualLink <Samack1011@gmail.com>
2026-04-06 00:05:11 -0500

    Clean up navigation bar styling to more closely match the paper prototype.

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,34 @@ function App () {
     return (
         <div>
             <div className={"navigationBar"}>
-                <Button onClick={() => setCurrentPage(0)}>{"Home"}</Button>
-                <Button onClick={() => setCurrentPage(1)}>{"Getting Started"}</Button>
-                <Button onClick={() => setCurrentPage(2)}>{"Buying Options"}</Button>
-                <Button onClick={() => setCurrentPage(3)}>{"Hidden Costs"}</Button>
+                <Button
+                    className={`navigationButton ${currentPage === 0 ? "active" : ""}`}
+                    onClick={() => setCurrentPage(0)}
+                    variant={"contained"}
+                >
+                    {"Home"}
+                </Button>
+                <Button
+                    className={`navigationButton ${currentPage === 1 ? "active" : ""}`}
+                    onClick={() => setCurrentPage(1)}
+                    variant={"contained"}
+                >
+                    {"Getting Started"}
+                </Button>
+                <Button
+                    className={`navigationButton ${currentPage === 2 ? "active" : ""}`}
+                    onClick={() => setCurrentPage(2)}
+                    variant={"contained"}
+                >
+                    {"Buying Options"}
+                </Button>
+                <Button
+                    className={`navigationButton ${currentPage === 3 ? "active" : ""}`}
+                    onClick={() => setCurrentPage(3)}
+                    variant={"contained"}
+                >
+                    {"Hidden Costs"}
+                </Button>
             </div>
             <div className={"pageContent"}>
                 {currentPage === 0 && (
