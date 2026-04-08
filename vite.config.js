91ffaf9ed8a76466a8d2a242fa25e6c1e977f890
PerpetualLink <Samack1011@gmail.com>
2026-04-07 19:20:33 -0500

    Fix repo name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PerpetualLink/CS422',
+  base: '/CS422/',
   plugins: [react()],
 }); 
